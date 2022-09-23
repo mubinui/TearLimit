c9c5a03ca4caaac9bf7561b719b5aebab2ab7e62
uicmu <mubin41@student.sust.edu>
2022-09-24 04:08:37 +0600

    Somehow it is done

--- src/Test/Test02.java
+++ src/Test/Test02.java
@@ -1,9 +1,16 @@
 package Test;
 
+import java.util.Arrays;
+
 public class Test02 {
     public static void main(String[] args) {
         String x = "Mubin";
         x = "Samindra";
         System.out.println(x);
+        int a [] = {7,8,9,1,2,3};
+        Arrays.sort(a);
+        for(int i:a){
+            System.out.println(i+" ");
+        }
     }
 }
